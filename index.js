8a670b9a672349867e5235d451bf63dfdda20be5
Tomáš Valenta <valenta.and.thomas@gmail.com>
2020-08-16 10:39:42 +0200

    Add basic concurrency handling

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-function middleware(request, response, next) {
+const axios = require('axios').default;
 
+async function httpsMiddleware(request, response, next) {
+  const warmUp = request.body.warmUp;
+  if (warmUp) {
+    const { concurrency, url } = warmUp;
+
+    if (concurrency > 1) {
+      try {
+        const result = await axios.post(url, {
+          ...request.body,
+          warmUp: {
+            ...warmUp,
+            concurrency: warmUp.concurrency - 1,
+          }
+        });
+        response.status(204).send(`Warm up instance: ${concurrency}`);
+      } catch (error) {
+        response.status(500).send(`Warm up error: ${error.message}`);
+      }
+    } else {
+      response.status(204).send(`Warm up instance: ${concurrency}`);
+    }
+  } else {
+    next(request, response);
+  }
 }
 
-exports.middleware = middleware;
+module.export = httpsMiddleware;
